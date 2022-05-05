abead7eb24926f5cb4f4eed25871bf9bb0e6a06e
rajeshtechstack <rajeshb7631@gmail.com>
2022-05-05 21:19:40 +0530

    Added user oauth module

---
+++ estock-user-oauth-service/src/main/java/com/estock/market/EstockUserOauthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.estock.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstockUserOauthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstockUserOauthServiceApplication.class, args);
+    }
+
+}
---
+++ estock-user-oauth-service/src/test/java/com/estock/market/EstockUserOauthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estock.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstockUserOauthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
